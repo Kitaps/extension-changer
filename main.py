e64c262b6a432160b93ee40310b429632955d830
Patrick Liedtke <patrick.liedtke@emasa.cl>
2023-12-19 11:16:17 -0300

    add console input to make it executable

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from os import rename, listdir
 
-old_extension = ".txt"
-new_extension = ".csv"
+old_extension = input("Enter the old extension: ")
+new_extension = input("Enter the new extension: ")
 
 local_file_path_names_list = listdir()
 for path in local_file_path_names_list:
-    new_path = path.replace(old_extension, new_extension)
-    rename(path, new_path)
+    if(old_extension in path):
+        new_path = path.replace(old_extension, new_extension)
+        rename(path, new_path)
     
     
